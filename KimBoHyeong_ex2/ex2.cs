c51f35c8aae9919a67e5c099c5e7e44806e2a6bf
bohyeong99 <bohyeong99@gmail.com>
2023-03-30 13:50:54 +0900

    03/30

---
+++ KimBoHyeong_ex2/ex2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex2
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, mean);
+            Console.WriteLine("평균: {0}", mean);
+        }
+
+        public static void Mean(
+            double a, double b, double c,
+            double d, double e, double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
